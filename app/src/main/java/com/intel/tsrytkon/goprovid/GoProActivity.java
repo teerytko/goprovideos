ff227605f5d2835cd87dcf7535a14bcd4709142d
tsrytkon <teemu.s.rytkonen@intel.com>
2014-12-26 19:28:58 +0200

    Add support for opening video file from main view

--- app/src/main/java/com/intel/tsrytkon/goprovid/GoProActivity.java
+++ app/src/main/java/com/intel/tsrytkon/goprovid/GoProActivity.java
@@ -6,6 +6,7 @@
 
 package com.intel.tsrytkon.goprovid;
 
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -18,20 +19,23 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.support.v4.app.LoaderManager;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 
 public class GoProActivity extends FragmentActivity implements LoaderManager.LoaderCallbacks<Cursor> {
     private static final String MEDIA = "media";
     private ListView mListView;
-    private TextView mStatusView;
+    private ArrayList mPaths = new ArrayList();
     private static String STREAM_VIDEO = "https://www.youtube.com/watch?v=z0JebT4kIPU";
     private static String FILE_VIDEO = "/storage/sdcard0/DCIM/100ANDRO/VID_0010.mp4";
     //private static String FILE_VIDEO = "storage/extSdCard/DCIM/Camera/20141224_135804.mp4";
@@ -43,7 +47,10 @@ public class GoProActivity extends FragmentActivity implements LoaderManager.Loa
     @Override
     public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
         Log.i(TAG, "onCreateLoader");
-        String[] projection = {MediaStore.Video.Media._ID, MediaStore.Video.Media.DISPLAY_NAME};
+        String[] projection = {MediaStore.Video.Media._ID,
+                               MediaStore.Video.Media.DISPLAY_NAME,
+                               MediaStore.Video.Media.DATE_ADDED,
+                               MediaStore.Video.Media.DATA};
         return new CursorLoader(this, MediaStore.Video.Media.EXTERNAL_CONTENT_URI, projection,
                 null, // Return all rows
                 null, null);
@@ -52,6 +59,13 @@ public class GoProActivity extends FragmentActivity implements LoaderManager.Loa
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
         Log.i(TAG, "onLoadFinished");
+        cursor.moveToFirst();
+        int index = 0;
+        do {
+            Log.i(TAG, cursor.getString(0) + " " + cursor.getString(1)+" " + cursor.getString(3));
+            mPaths.add(index++, cursor.getString(3));
+        } while(cursor.moveToNext());
+        cursor.moveToFirst();
         mAdapter.changeCursor(cursor);
     }
 
@@ -67,19 +81,32 @@ public class GoProActivity extends FragmentActivity implements LoaderManager.Loa
         setContentView(R.layout.activity_go_pro);
         // get reference to the views
         mListView = (ListView) findViewById(R.id.listView);
-        mStatusView = (TextView) findViewById(R.id.statusView);
         Log.i(TAG, "Create main view!!");
         // For the cursor adapter, specify which columns go into which views
-        String[] fromColumns = {MediaStore.Video.Media.DISPLAY_NAME};
-        int[] toViews = {android.R.id.text1}; // The TextView in simple_list_item_1
+        String[] fromColumns = {MediaStore.Video.Media.DISPLAY_NAME,
+                                MediaStore.Video.Media.DATE_ADDED};
+        int[] toViews = {android.R.id.text1, android.R.id.text2}; // The TextView in simple_list_item_2
 
         // Create an empty adapter we will use to display the loaded data.
         // We pass null for the cursor, then update it in onLoadFinished()
         mAdapter = new SimpleCursorAdapter(this,
-                android.R.layout.simple_list_item_1, null,
+                android.R.layout.simple_list_item_2, null,
                 fromColumns, toViews, 0);
 
         mListView.setAdapter(mAdapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1,int position, long id)
+            {
+                String videopath = (String) mPaths.get(position);
+                Toast.makeText(GoProActivity.this, videopath, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(GoProActivity.this,
+                        FramePlayerVideoActivity.class);
+                intent.putExtra(MEDIA, videopath);
+                startActivity(intent);
+            }
+        });
         getSupportLoaderManager().initLoader(1, null, this);
     }
 
